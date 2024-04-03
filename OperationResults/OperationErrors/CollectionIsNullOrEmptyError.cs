12080aa71920d459c1b1764a90ceff37d151a7b6
Chukashev Oleg <chukoleg@gmail.com>
2024-04-03 18:54:39 +0300

    Fix argument nre in CollectionIsNullOrEmptyError

--- OperationResults/OperationErrors/CollectionIsNullOrEmptyError.cs
+++ OperationResults/OperationErrors/CollectionIsNullOrEmptyError.cs
@@ -2,9 +2,9 @@
 
 namespace OperationResults.OperationErrors
 {
-    public class CollectionIsNullOrEmptyError : OperationError
+    public class CollectionIsNullOrEmptyError<TCollection> : OperationError where TCollection : IEnumerable
     {
         private CollectionIsNullOrEmptyError(string message = null) : base(message) { }
-        public CollectionIsNullOrEmptyError(IEnumerable collection) : base($"Collection is null or empty. Collection type: {collection.GetType().FullName}") { }
+        public CollectionIsNullOrEmptyError(TCollection _, string argumentName = null) : base($"Collection {argumentName} of type {typeof(TCollection).FullName} is null or empty") { }
     }
 }
